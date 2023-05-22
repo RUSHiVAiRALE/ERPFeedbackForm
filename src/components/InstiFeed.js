abe8e0fd1a46be3b1dbb7f23c63e0f93eab3487a
RUSHi-VAiRALE <rushivairale932@gmail.com>
2023-05-22 12:34:00 +0530

    Demo from

---
+++ src/components/InstiFeed.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+const InstituitionalFeed = () => {
+    return (
+        <div>
+            <div class="wrapper centered">
+                <article class="letter">
+                <div class="side">
+                    <h1>Instituitional Feedback Form</h1>
+                    {/* our form starts from here */}
+                </div>
+                {/* <div class="side">
+                    <p>
+                    <input type="text" placeholder="Your name" />
+                    </p>
+                    <p>
+                    <input type="email" placeholder="Your email" />
+                    </p>
+                    <p>
+                    <button id="sendLetter">Send</button>
+                    </p>
+                </div> */}
+                </article>
+                <div class="envelope front"></div>
+            <div class="envelope back"></div>
+            </div>
+        </div>
+    );
+}
+
+export default InstituitionalFeed;
